d45f57fb0d0914dceb3515624d1621d9200ac7fe
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-10-13 18:26:15 +0300

    Add: new router

--- src/psychologicalbot/main.py
+++ src/psychologicalbot/main.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher
 from core import load_config
-from psychologicalbot.handlers import start_router
+from psychologicalbot.handlers import start_router, register_router
 
 config = load_config()
 bot = Bot(token=config.bot_token)
@@ -9,6 +9,8 @@ bot = Bot(token=config.bot_token)
 dp = Dispatcher()
 
 dp.include_router(start_router)
+dp.include_router(register_router)
+
 
 async def main():
     logging.basicConfig(level=logging.INFO)
