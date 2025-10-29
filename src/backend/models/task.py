3f122c9025c62ca8364bf4138385e45e3c7e6565
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-10-29 16:50:38 +0300

    Modifed task model

--- src/backend/models/task.py
+++ src/backend/models/task.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Column, Integer, String, Float, Text, BigInteger
+from sqlalchemy import Column, Integer, String, Float, Text, BigInteger, Boolean
 from backend.models.base import Base
+from dataclasses import dataclass
 
+@dataclass  
 class Task(Base):
     __tablename__ = "tasks"
 
     taks_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    task_name = Column(String(100), nullable=False)
+    title = Column(String(100), nullable=False)
     description = Column(Text, nullable=False)
+    complited = Column(Boolean, default=False)
+    category = Column(String(50), nullable=False)
 
     def __repr__(self):
-        return f"<Task task_id= {self.taks_id}, task_name= {self.taks_name} added>"
+        return f"<Task task_id= {self.taks_id}, title= {self.title} added>"
