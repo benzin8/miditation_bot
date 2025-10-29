ba224707069d6a506defe845afa18b8d9f6d45df
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-10-29 16:53:39 +0300

    Modifed get session, added yield session for api

--- src/core/database.py
+++ src/core/database.py
@@ -11,3 +11,7 @@ async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+async def get_session_local():
+    async with async_session() as session:
+        yield session
