aefaabf056bb715759832ce2f34c2f409cd5cba2
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-11-07 19:16:22 +0300

    executer func for task

--- src/backend/services/task_service.py
+++ src/backend/services/task_service.py
@@ -10,4 +10,17 @@ async def get_random_task() -> Task:
         )
         task = result.scalar_one_or_none()
         return task
-    
+
+async def execute_task(task_id):
+    async with async_session() as session:
+        result = await session.execute(
+            select(Task).where(Task.task_id == task_id)
+        )
+        task = result.scalar_one_or_none()
+
+        if task is None:
+            raise f"Task {task_id} not found"
+
+        task.complited = True
+        await session.commit()
+        return task
